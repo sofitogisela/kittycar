331aa3a728eb629c57fa1f2545d9a4030ab9048d
Daniel Eleazar Fragoso Reyes <dreyes@MacBook-Pro-de-Daniel-2.local>
2024-11-03 17:35:46 -0600

    avanzar5

--- motors_test.py
+++ motors_test.py
@@ -1,25 +1,17 @@
 import gpiozero
 from time import sleep
 
-motor_left = gpiozero.Motor(forward=5, backward=6)
+# Updated motor configuration
+motor_left = gpiozero.Motor(forward=12, backward=6)
 motor_right = gpiozero.Motor(forward=13, backward=19)
+speed = 0.5  # Set speed (0 to 1 scale)
 
 print("Testing left motor forward")
-motor_left.forward(0.5)
+motor_left.forward(speed)
 sleep(2)
 motor_left.stop()
 
 print("Testing right motor forward")
-motor_right.forward(0.5)
-sleep(2)
-motor_right.stop()
-
-print("Testing left motor backward")
-motor_left.backward(0.5)
-sleep(2)
-motor_left.stop()
-
-print("Testing right motor backward")
-motor_right.backward(0.5)
+motor_right.forward(speed)
 sleep(2)
 motor_right.stop()
